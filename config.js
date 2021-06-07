ba8937ba7787c3cc54496fbdfbb0d74a1a9941b4
cuongngo1121 <85379795+cuongngo1121@users.noreply.github.com>
2021-06-07 09:01:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'cá dọn bể à :))!',
+    introTitle: 'Đồng chí 11A2 à :))!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100024728014963' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/cuong.1124' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
